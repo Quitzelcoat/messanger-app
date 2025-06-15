5457ef525fb2ba8393f1e8f90da89cfe2aa3bda8
Haris Saeed <96650020+Quitzelcoat@users.noreply.github.com>
2025-06-14 22:24:03 -0400

    backend signup login completed, frontend next

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome');
 });
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀Server running on http://localhost:${PORT}`);
 });
---
+++ backend/controllers/authController.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('../generated/prisma');
+const prisma = new PrismaClient();
+
+exports.signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+      },
+    });
+
+    return res.status(201).json({ user });
+  } catch (error) {
+    console.log('Signup error:', error);
+    if (error.code === 'P2002') {
+      return res
+        .status(400)
+        .json({ error: 'Username or email already exists' });
+    }
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        password: true,
+      },
+    });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Invalid email or password' });
+    }
+
+    const payload = {
+      userId: user.id,
+      username: user.username,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+  } catch (error) {
+    console.log('Login error:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
